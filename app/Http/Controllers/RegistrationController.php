f36d7df8de58921a006b0d654600f230de986c60
loudislav <loudislav@gmail.com>
2018-10-27 13:21:07 +0200

    another solution for PostgreSQL

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -33,7 +33,7 @@ class RegistrationController extends Controller
 
     public function showByUser($id)
     {
-        return response()->json(Registration::groupBy('name', 'surname', 'birthdate')->having('registered_by', $id)->having('event', 'like', '2%')->orderBy('surname', 'asc')->get());
+        return response()->json(Registration::select('name', 'surname', 'birthdate', 'id_number', 'street', 'city', 'zip', 'note')->where('registered_by', $id)->where('event', 'like', '2%')->groupBy('name', 'surname', 'birthdate')->orderBy('surname', 'asc')->get());
     }
 
     public function showByEvent($id)
