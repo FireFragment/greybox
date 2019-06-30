e7d8a08e01297c1ed01241506e8d5949443bfb72
ApollonCZ <burgr.ladislav@gmail.com>
2019-06-30 12:34:45 +0200

    Debaters separated

--- src/pages/Tournament.vue
+++ src/pages/Tournament.vue
@@ -7,39 +7,7 @@
     </p>
     <div class="row q-col-gutter-md reverse">
       <div class="col-12 col-sm-4 col-md-6">
-        <q-list
-          class="rounded-borders shadow-2 q-pb-sm bg-white debaters-sticky-card"
-        >
-          <p class="text-center q-pb- q-pt-md q-pl-sm q-pr-sm">
-            Převyplnit údaje dle dříve zadaných osob:
-          </p>
-          <q-scroll-area style="height: calc(100vh - 130px);">
-            <q-item
-              clickable
-              v-ripple
-              dense
-              v-for="(pastLogin, index) in pastLogins"
-              v-bind:key="index"
-              class="q-pt-sm q-pb-sm"
-            >
-              <q-item-section>{{ pastLogin }}</q-item-section>
-              <q-item-section avatar>
-                <q-avatar
-                  :style="
-                    'background-color: ' + stringToHslColor(pastLogin, 50, 60)
-                  "
-                  size="30px"
-                >
-                  <img
-                    src="https://cdn.quasar.dev/img/avatar.png"
-                    v-if="!true"
-                  />
-                  <template v-else>{{ pastLogin.substr(0, 1) }}</template>
-                </q-avatar>
-              </q-item-section>
-            </q-item>
-          </q-scroll-area>
-        </q-list>
+        <debatersCard />
       </div>
       <q-form class="col-12 col-sm-8 col-md-6">
         <div class="row q-col-gutter-md q-pb-sm">
@@ -312,8 +280,13 @@
 </template>
 
 <script>
+import debatersCard from "../layouts/components/DebatersCard";
+
 export default {
   name: "PageTournament",
+  components: {
+    debatersCard
+  },
   data() {
     return {
       firstname: null,
@@ -348,34 +321,7 @@ export default {
         "listopad",
         "prosinec"
       ],
-      years: [],
-      pastLogins: [
-        "Adam Karásek",
-        "Jakub Szymsze",
-        "Ladislav Burgr",
-        "Vašek",
-        "Zbyněk",
-        "Jakub Szymsze",
-        "Ladislav Burgr",
-        "Vašek",
-        "Zbyněk",
-        "Jakub Szymsze",
-        "Ladislav Burgr",
-        "Vašek",
-        "Zbyněk",
-        "Jakub Szymsze",
-        "Ladislav Burgr",
-        "Vašek",
-        "Zbyněk",
-        "Jakub Szymsze",
-        "Ladislav Burgr",
-        "Vašek",
-        "Zbyněk",
-        "Jakub Szymsze",
-        "Ladislav Burgr",
-        "Vašek",
-        "Zbyněk"
-      ]
+      years: []
     };
   },
   mounted: function() {
