fc898d1376e94198303a501415836d489c634816
loudislav <loudislav@gmail.com>
2019-06-30 02:04:49 +0200

    authorization header

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,6 +31,10 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
+            if ($request->header('Authorization')) {
+                return User::where('api_token', $request->header('Authorization'))->first();
+            }
+            // TODO: Backwards compatibility. To be deleted later.
             if ($request->input('api_token')) {
                 return User::where('api_token', $request->input('api_token'))->first();
             }
