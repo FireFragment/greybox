1ec2c38b2ad3fd3b81d863015845d24f0ae61a31
ApollonCZ <burgr.ladislav@gmail.com>
2019-09-01 23:46:34 +0200

    HP buttons

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,26 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Logo ADK" src="../assets/logo_napis.png" />
+    <div class="row full-width text-center">
+      <img class="q-mx-auto" alt="Logo ADK" src="../assets/logo_napis.png" />
+    </div>
+    <div class="row flex-top">
+      <q-btn class="q-mx-md sign-in-button" push>
+        <div class="row items-center no-wrap">
+          <q-icon left name="fas fa-sign-in-alt" />
+          <div class="text-center">
+            Přihlásit se
+          </div>
+        </div>
+      </q-btn>
+      <q-btn class="q-mx-md register-button" push>
+        <div class="row items-center no-wrap">
+          <q-icon left name="fas fa-user-plus" />
+          <div class="text-center">
+            Registrace
+          </div>
+        </div>
+      </q-btn>
+    </div>
   </q-page>
 </template>
 
