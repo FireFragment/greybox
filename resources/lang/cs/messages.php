2415ed8b15e691cb4ca3913ad35b7809c257d0df
loudislav <loudislav@gmail.com>
2019-09-02 21:39:41 +0200

    correct reset password link

--- resources/lang/cs/messages.php
+++ resources/lang/cs/messages.php
@@ -7,5 +7,6 @@ return [
 
     'password.reset.subject' => 'Greybox - obnova hesla',
     'password.reset.before_link' => 'Dostali jsme Vaši žádost o obnovu hesla. Pro vytvoření nového použijte tento odkaz',
+    'password.reset.link' =>  'obnovit-heslo',
     'password.reset.after_link' => 'Pokud jste o obnovu hesla nežádal(a), můžete tuto zprávu ignorovat.'
 ];
--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -7,5 +7,6 @@ return [
 
     'password.reset.subject' => 'Greybox - password recovery',
     'password.reset.before_link' => "We have received your request for password recovery. To create a new password, use the following link",
+    'password.reset.link' =>  'password-reset',
     'password.reset.after_link' => 'If you haven\'t requested password recovery, you can ignore this email.'
 ];
