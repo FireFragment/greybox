3fae496969a8952f218c8092359612979131d025
loudislav <loudislav@gmail.com>
2020-04-28 21:25:04 +0200

    queue worker for jobs

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: vendor/bin/heroku-php-apache2 public/
 web: php -S 0.0.0.0:$PORT -t public
+worker: php artisan queue:restart && php artisan queue:work
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,13 +21,13 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new FakturoidClientUpdateJob(new FakturoidClientService()))
             ->description('Update clients data from Fakturoid subjects.')
-            ->everyMinute();
+            ->daily();
     }
 }
