a31148a2b99217983ad053ef3125f53c89d35a2e
Jakub Szymsza <jakub@szymsza.cz>
2021-07-20 19:13:12 +0200

    Use Quasar treeshaking

--- src/quasar.js
+++ src/quasar.js
@@ -1,13 +1,75 @@
 import Vue from "vue";
 
 import "./styles/quasar.styl";
-import "quasar/dist/quasar.ie.polyfills";
 import lang from "quasar/lang/cs.js";
 import "@quasar/extras/fontawesome-v5/fontawesome-v5.css";
 import "@quasar/extras/material-icons/material-icons.css";
-import Quasar from "quasar";
+import Quasar, {
+  QAvatar,
+  QBanner,
+  QBtn,
+  QBtnDropdown,
+  QCard,
+  QCardActions,
+  QCardSection,
+  QCheckbox,
+  QDialog,
+  QDrawer,
+  QForm,
+  QHeader,
+  QIcon,
+  QInnerLoading,
+  QInput,
+  QItem,
+  QItemLabel,
+  QItemSection,
+  QLayout,
+  QList,
+  QPage,
+  QPageContainer,
+  QScrollArea,
+  QSelect,
+  QSeparator,
+  QSpace,
+  QSpinner,
+  QToolbar,
+  QToolbarTitle,
+  QTooltip
+} from "quasar";
 
 Vue.use(Quasar, {
+  components: {
+    QAvatar,
+    QBanner,
+    QBtn,
+    QBtnDropdown,
+    QCard,
+    QCardActions,
+    QCardSection,
+    QCheckbox,
+    QDialog,
+    QDrawer,
+    QForm,
+    QHeader,
+    QIcon,
+    QInnerLoading,
+    QInput,
+    QItem,
+    QItemLabel,
+    QItemSection,
+    QLayout,
+    QList,
+    QPage,
+    QPageContainer,
+    QScrollArea,
+    QSelect,
+    QSeparator,
+    QSpace,
+    QSpinner,
+    QToolbar,
+    QToolbarTitle,
+    QTooltip
+  },
   config: {
     loadingBar: {
       color: "light-blue-5"
