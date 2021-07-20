9fd690b3480467215d9490d2b6d8f73843d3b4d1
Jakub Szymsza <jakub@szymsza.cz>
2021-07-20 20:47:59 +0200

    Add env files etc

--- quasar.conf.js
+++ quasar.conf.js
@@ -12,7 +12,7 @@
 /* eslint global-require: 0 */
 const { configure } = require('quasar/wrappers');
 
-module.exports = configure((ctx) => ({
+module.exports = configure(() => ({
   // https://v2.quasar.dev/quasar-cli/supporting-ts
   supportTS: {
     tsCheckerConfig: {
@@ -56,6 +56,8 @@ module.exports = configure((ctx) => ({
   // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
   build: {
     vueRouterMode: 'hash', // available values: 'hash', 'history'
+    publicPath: process.env.VUE_APP_BASE_ROUTE,
+    distDir: process.env.VUE_APP_BUILD_DIR,
 
     // transpile: false,
 
@@ -107,123 +109,5 @@ module.exports = configure((ctx) => ({
 
   // animations: 'all', // --- includes all animations
   // https://v2.quasar.dev/options/animations
-  animations: [],
-
-  // https://v2.quasar.dev/quasar-cli/developing-ssr/configuring-ssr
-  ssr: {
-    pwa: false,
-
-    // manualStoreHydration: true,
-    // manualPostHydrationTrigger: true,
-
-    prodPort: 3000, // The default port that the production server should use
-    // (gets superseded if process.env.PORT is specified at runtime)
-
-    maxAge: 1000 * 60 * 60 * 24 * 30,
-    // Tell browser when a file from the server should expire from cache (in ms)
-
-    chainWebpackWebserver(/* chain */) {
-      //
-    },
-
-    middlewares: [
-      ctx.prod ? 'compression' : '',
-      'render', // keep this as last one
-    ],
-  },
-
-  // https://v2.quasar.dev/quasar-cli/developing-pwa/configuring-pwa
-  pwa: {
-    workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
-    workboxOptions: {}, // only for GenerateSW
-
-    // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
-    // if using workbox in InjectManifest mode
-    chainWebpackCustomSW(/* chain */) {
-      //
-    },
-
-    manifest: {
-      name: 'Greybox 2.0',
-      short_name: 'Greybox 2.0',
-      description: 'Greybox 2.0',
-      display: 'standalone',
-      orientation: 'portrait',
-      background_color: '#ffffff',
-      theme_color: '#027be3',
-      icons: [
-        {
-          src: 'icons/icon-128x128.png',
-          sizes: '128x128',
-          type: 'image/png',
-        },
-        {
-          src: 'icons/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: 'icons/icon-256x256.png',
-          sizes: '256x256',
-          type: 'image/png',
-        },
-        {
-          src: 'icons/icon-384x384.png',
-          sizes: '384x384',
-          type: 'image/png',
-        },
-        {
-          src: 'icons/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-    },
-  },
-
-  // Full list of options: https://v2.quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
-  cordova: {
-    // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
-  },
-
-  // Full list of options: https://v2.quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
-  capacitor: {
-    hideSplashscreen: true,
-  },
-
-  // Full list of options: https://v2.quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
-  electron: {
-    bundler: 'packager', // 'packager' or 'builder'
-
-    packager: {
-      // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
-      // OS X / Mac App Store
-      // appBundleId: '',
-      // appCategoryType: '',
-      // osxSign: '',
-      // protocol: 'myapp://path',
-
-      // Windows only
-      // win32metadata: { ... }
-    },
-
-    builder: {
-      // https://www.electron.build/configuration/configuration
-
-      appId: 'greybox',
-    },
-
-    // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-    chainWebpack(/* chain */) {
-      // do something with the Electron main process Webpack cfg
-      // extendWebpackMain also available besides this chainWebpackMain
-    },
-
-    // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-    chainWebpackPreload(/* chain */) {
-      // do something with the Electron main process Webpack cfg
-      // extendWebpackPreload also available besides this chainWebpackPreload
-    },
-  },
+  animations: []
 }));
