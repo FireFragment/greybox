767cc7407d07e741a68af4c456232037337acdd5
ApollonCZ <burgr.ladislav@gmail.com>
2021-07-22 00:36:43 +0200

    go back button update

--- src/pages/Event.vue
+++ src/pages/Event.vue
@@ -2,8 +2,8 @@
   <q-page padding v-if="event">
     <!-- Header card -->
     <div class="text-center">
-      <q-btn color="white" text-color="black" class="float-left q-mb-md" @click="goBack" v-if="type">
-        <q-icon size="2em" name="fas fa-arrow-left" />
+      <q-btn color="white" text-color="black" class="float-left q-mb-md go-back-button" @click="goBack" v-if="type">
+        <q-icon size="2em" name="fas fa-arrow-left " />
       </q-btn>
       <q-card
         class="inline-block event-header"
