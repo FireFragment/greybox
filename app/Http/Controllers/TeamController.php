4fd1fb8e2af74ac4572f1c18037627b71c3dc3fa
loudislav <loudislav@gmail.com>
2021-07-22 02:53:00 +0200

    solving Bugsnag Illuminate\Database\QueryExceptionGET /api/team/undefined

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -27,7 +27,7 @@ class TeamController extends Controller
 
     public function showOne($id)
     {
-        $team = Team::find($id);
+        $team = Team::findOrFail($id);
         // TODO: take into account the most recent tournaments
         $registrations = $team->registrations()->select(\DB::raw('person, count(*) as attendance'))->whereNotNull('person')->groupBy('person')->orderBy('attendance', 'DESC')->limit(5)->get();
 
