8b50f0bdba53c8a1d85f1a1f18f5b277390dc236
ApollonCZ <burgr.ladislav@gmail.com>
2021-08-23 14:08:16 +0200

    current registrations progress

--- src/pages/Auth/CurrentRegistrations.vue
+++ src/pages/Auth/CurrentRegistrations.vue
@@ -3,6 +3,7 @@
     <h1 class="text-center text-h4">{{ $tr('currentRegistrations.title') }}</h1>
 
     <div class="row">
+
     </div>
 
   </q-page>
@@ -17,39 +18,99 @@ interface CurrentRegistrationsData {
   eventId: number;
 }
 
+interface EventPersonRegistrations {
+  id: number;
+  person: {
+    id: number;
+    name: string;
+    surname: string;
+    birthdate: string;
+    // eslint-disable-next-line camelcase
+    id_number: string|null;
+    street: string;
+    city: string;
+    zip: string;
+    school: string|null;
+    note: string|null;
+    // eslint-disable-next-line camelcase
+    created_at: string;
+    // eslint-disable-next-line camelcase
+    updated_at: string;
+  },
+  note: string|null;
+  event: string;
+  // eslint-disable-next-line camelcase
+  event_id: number;
+  role: {
+    id: number;
+    name: {
+      id: number;
+      cs: string;
+      en: string;
+      // eslint-disable-next-line camelcase
+      created_at: string;
+      // eslint-disable-next-line camelcase
+      updated_at: string;
+    },
+    icon: string|null;
+    // eslint-disable-next-line camelcase
+    created_at: string;
+    // eslint-disable-next-line camelcase
+    updated_at: string;
+  },
+  accommodation: number;
+  confirmed: number;
+  team: string|null;
+  // eslint-disable-next-line camelcase
+  registered_by: number;
+  invoice: string|null;
+  // eslint-disable-next-line camelcase
+  created_at: string;
+  // eslint-disable-next-line camelcase
+  updated_at: string;
+}
+
 export default defineComponent({
   name: 'CurrentRegistrations',
   data(): CurrentRegistrationsData {
     return {
       translationPrefix: 'auth.',
-      eventId: 41,
+      people: [],
     };
   },
   created() {
     const DBkey = 'current-registrations';
     const cached: DBValue = this.$db(DBkey);
     if (cached) {
-      // TODO - save cached data
-      // this.data = cached;
+      this.people = cached.people;
       return;
     }
 
     this.$bus.$emit('fullLoader', true);
-    this.$api({
-      url: `event/${this.eventId}/user/${this.$auth.user()!.id}/registration`,
-      method: 'get',
-    })
-      .then(({ data }) => {
-        // TODO - save loaded data
-        // this.data = data;
-        console.log(data);
-        // assertDBValue(data);
-        this.$db(DBkey, data, true);
+
+    const events = this.$db('eventsList');
+    const eventIds = Object.keys(events);
+    eventIds.forEach(function (eventId) {
+      console.log(eventId);
+      this.$api({
+        url: `event/${eventId}/user/${this.$auth.user()!.id}/registration`,
+        method: 'get',
       })
-      .finally(() => {
-        console.log('cs');
-        this.$bus.$emit('fullLoader', false);
-      });
+        .then(({ data }) => {
+          // TODO - save loaded data
+          // this.people = data;
+          console.log(data);
+
+          // assertDBValue(data);
+          this.$db(DBkey, data, true);
+        })
+        .finally(() => {
+          console.log('cs');
+
+          this.$bus.$emit('fullLoader', false);
+        });
+    });
+
   },
 });
 </script>
