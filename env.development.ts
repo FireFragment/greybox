5d77d00044732f2e73409ab82c143a9525a88958
Jakub Szymsza <jakub@szymsza.cz>
2022-11-24 17:09:48 +0100

    Change debug API URL

--- env.development.ts
+++ env.development.ts
@@ -2,7 +2,7 @@ module.exports = {
   MODE: 'development',
   IS_PDS: 'false',
   BASE_ROUTE: '/',
-  API_URL: 'https://debate-greybox-stage.herokuapp.com/',
+  API_URL: 'https://debata21.cz/',
   BUILD_DIR: 'registrace_dev',
   STAGE: 'local',
   BUGSNAG_KEY: null,
--- env.pds-dev.ts
+++ env.pds-dev.ts
@@ -2,7 +2,7 @@ module.exports = {
   MODE: 'development',
   IS_PDS: 'true',
   BASE_ROUTE: '/',
-  API_URL: 'https://debate-greybox-stage.herokuapp.com/',
+  API_URL: 'https://debata21.cz/',
   BUILD_DIR: 'registration_dev',
   STAGE: 'pds-local',
   BUGSNAG_KEY: null,
