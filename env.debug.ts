a1eab88c86e453ef8600eec04fa706fc26272958
Jakub Szymsza <jakub@szymsza.cz>
2022-11-27 15:10:09 +0100

    Prepare debug build to run on VPS

--- env.debug.ts
+++ env.debug.ts
@@ -1,9 +1,9 @@
 module.exports = {
   MODE: 'debug',
   IS_PDS: 'false',
-  BASE_ROUTE: '/debugbox/',
+  BASE_ROUTE: '/',
   API_URL: 'https://api.debata21.cz/',
-  BUILD_DIR: 'debugbox',
+  BUILD_DIR: 'build-debug',
   STAGE: 'debug',
   BUGSNAG_KEY: 'c5988a8332d9e7594c4c5986484f50a8',
 };
