87c33c90debc9bc9f364bd159545d90938360227
Jakub Szymsza <jakub@szymsza.cz>
2022-12-05 22:19:29 +0100

    Connect production build to VPS API

--- env.pds-prod.ts
+++ env.pds-prod.ts
@@ -2,7 +2,7 @@ module.exports = {
   MODE: 'production',
   IS_PDS: 'true',
   BASE_ROUTE: '/pds/registration/',
-  API_URL: 'https://debate-greybox.herokuapp.com/',
+  API_URL: 'https://api-prod.debata21.cz/',
   BUILD_DIR: 'registration',
   STAGE: 'pds',
   BUGSNAG_KEY: 'c5988a8332d9e7594c4c5986484f50a8',
