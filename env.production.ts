1a169f6be1ae7e2edf9d95209c258706ebdf8fb0
Jakub Szymsza <jakub@szymsza.cz>
2022-12-06 11:09:34 +0100

    Remove redundant slash from API URL

--- env.production.ts
+++ env.production.ts
@@ -2,7 +2,7 @@ module.exports = {
   MODE: 'production',
   IS_PDS: 'false',
   BASE_ROUTE: '/greybox/registrace/',
-  API_URL: 'https://api-prod.debata21.cz//',
+  API_URL: 'https://api-prod.debata21.cz/',
   BUILD_DIR: 'registrace',
   STAGE: 'production',
   BUGSNAG_KEY: 'c5988a8332d9e7594c4c5986484f50a8',
