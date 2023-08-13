3b1a6c5fe87a68aa422af8199e1c5fda743e8bd7
loudislav <loudislav@gmail.com>
2023-08-13 15:45:37 +0200

    ORGANIZER_ROLE_ID to env

--- app/Policies/EventOrganizer.php
+++ app/Policies/EventOrganizer.php
@@ -5,13 +5,17 @@ namespace App\Policies;
 use App\Event;
 use App\User;
 
-const ORGANIZER_ROLE_ID = 4;
-
 trait EventOrganizer {
 
+    /**
+     * Check if the user is an organizer of the event.
+     * @param User $user
+     * @param Event $event
+     * @return bool
+     */
     public function isEventOrganizer(User $user, Event $event): bool
     {
-        $organizers = $event->registrations()->where('role', ORGANIZER_ROLE_ID)->get();
+        $organizers = $event->registrations()->where('role', getenv('ORGANIZER_ROLE_ID'))->get();
         $userPerson = $user->person()->first();
         foreach ($organizers as $organizer) {
             $organizerPerson = $organizer->person()->first();
