6a08e03e64e674ccb37be6dfa41bdc3824bca886
Jakub Szymsza <jakub@szymsza.cz>
2023-08-25 22:32:01 +0200

    New production route

--- env.production.ts
+++ env.production.ts
@@ -1,9 +1,9 @@
 module.exports = {
   MODE: 'production',
   IS_PDS: 'false',
-  BASE_ROUTE: '/greybox/registrace/',
+  BASE_ROUTE: '/',
   API_URL: 'https://api-prod.debata21.cz/',
-  BUILD_DIR: 'registrace',
+  BUILD_DIR: 'greybox',
   STAGE: 'production',
   BUGSNAG_KEY: 'c5988a8332d9e7594c4c5986484f50a8',
 };
